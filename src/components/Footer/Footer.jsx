d3172506aaaf533e70a51403ce4233b81d02b9dd
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-07-11 02:16:00 +0530

    Add activeLink class to nav links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='text-center'>Footer</footer>
+    <footer className='text-center'>
+      
+      <p>&copy; Aditya Mishra : my resum<span>&eacute;</span></p>
+    </footer>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { NavHashLink as NavLink } from "react-router-hash-link";
-import { navLinks } from "../../constants";
+import { Bio, navLinks } from "../../constants";
+import "./navbar.css";
 
 const Navbar = () => {
+  const handleNavClick = (e) => {
+    const navList = Array.from(document.getElementsByClassName("nav-link"));
+
+    navList.forEach((nav) => {
+      nav.classList.remove("activeLink");
+    });
+
+    e.target.parentElement.classList.add("activeLink");
+  };
   return (
-    <div id="navbar" className="navbar">
+    <div id="navbar" className="navbar py-4">
       <Router>
-        <nav>
+        <nav className="flex justify-between items-center">
+          <div className="my-name-nav-container text-2xl font-bold">
+            {`${Bio.firstName} ${Bio.lastName}`}
+          </div>
           <ul className="nav-links-container ml-auto flex w-3/6 justify-end items-center gap-2">
             {navLinks.map((nav) => (
-              <li className="nav-link">
-                <NavLink to={nav.id}>{nav.title}</NavLink>
+              <li className="nav-link text-xl text-gray-400 font-semibold">
+                <NavLink to={nav.id} onClick={(e) => handleNavClick(e)}>
+                  {nav.title}
+                </NavLink>
               </li>
             ))}
           </ul>
